44d661bd073648adb3c23af2afcd7e6aca907263
monimoyd <monimoyd@gmail.com>
2018-01-05 00:24:10 +0530

    Class for enriching and validating Music Data

--- MusicDataEnricher.scala
+++ MusicDataEnricher.scala
@@ -15,37 +15,21 @@ import scala.collection.mutable.HashMap
 	
 //val sc: SparkContext = context
 //class MusicDataEnricher( allDataFrameParam: DataFrame, stndIdGeoCdMapParam:HashMap[String, String], broadcastSongArtistMapParam:Broadcast[Map[String, String]], broadcastUserArtistMapParam: Broadcast[Map[String, String]], broadcastUserSubscriptionParam: Broadcast[Map[String, (Long, Long)]]) extends Serializable {
-class MusicDataEnricher( allDataFrameParam: DataFrame, broadcastStndIdGeoCdMapParam:Broadcast[HashMap[String, String]], broadcastSongArtistMapParam:Broadcast[Map[String, String]], broadcastUserArtistMapParam: Broadcast[Map[String, String]], broadcastUserSubscriptionParam: Broadcast[Map[String, (Long, Long)]]) extends Serializable {
+class MusicDataEnricher( allDataFrameParam: DataFrame, broadcastStndIdGeoCdMapParam:Broadcast[Map[String, String]], broadcastSongArtistMapParam:Broadcast[Map[String, String]], broadcastUserArtistMapParam: Broadcast[Map[String, String]], broadcastUserSubscriptionParam: Broadcast[Map[String, (Long, Long)]]) extends Serializable {
 	val allDataFrame: DataFrame =allDataFrameParam
  // val stndIdGeoCdMap:HashMap[String, String] = stndIdGeoCdMapParam
-	val broadcastStndIdGeoCdMap:Broadcast[HashMap[String, String]] = broadcastStndIdGeoCdMapParam
+	val broadcastStndIdGeoCdMap:Broadcast[Map[String, String]] = broadcastStndIdGeoCdMapParam
 	val broadcastSongArtistMap:Broadcast[Map[String, String]] = broadcastSongArtistMapParam
 	val broadcastUserArtistMap:Broadcast[Map[String, String]] = broadcastUserArtistMapParam
 	val broadcastUserSubscription:Broadcast[Map[String, (Long, Long)]] = broadcastUserSubscriptionParam
 
 	def fillNullValueGeoCd = udf((stationId: String, geoCd: String) => {
-	    val stndIdGeoCdMap1:HashMap[String, String] = HashMap("ST400" -> "A",
-                          "ST401" -> "AU",
-                          "ST402" -> "AP",
-                          "ST403" -> "J",
-                          "ST404" -> "E",
-                          "ST405" -> "A",
-                          "ST406" -> "AU",
-                           "ST407" -> "AP",
-                           "ST408" -> "E",
-                           "ST409" -> "E",
-                           "ST410" -> "A",
-                           "ST411"-> "A",
-                           "ST412" -> "AP",
-                           "ST413" -> "J",
-                           "ST41" -> "E"
-                      )
-                      
+	                       
 		if (!geoCd.equals("")) geoCd
 		else {
 		  //  val geoCdVal = stndIdGeoCdMap.get(stationId).getOrElse("Invalid")
 		  val geoCdVal = broadcastStndIdGeoCdMap.value.get(stationId).getOrElse("Invalid")
-			  geoCdVal
+			 geoCdVal
 		}
 	})
 
@@ -74,7 +58,7 @@ def findSubscribers = udf((userId: String, startTs: Long) => {
 
 	val subscriptionTuple= broadcastUserSubscription.value.get(userId).getOrElse((0L, 0L))
   if (subscriptionTuple._1 ==0 && subscriptionTuple._2 == 0 ) "0"
-	else if (startTs >= subscriptionTuple._2  && startTs <=  subscriptionTuple._1) "1"
+	else if (startTs >= subscriptionTuple._1  && startTs <=  subscriptionTuple._2) "1"
 	else "0"
 
 })
@@ -92,16 +76,13 @@ def validateRecords = udf((userId: String, songId: String, modifiedArtistId: Str
 
 
 
-
-
-
 def enrichData():DataFrame = {
 	 
  var newDataFrame = allDataFrame.withColumn("modified_Geo_cd",  fillNullValueGeoCd(allDataFrame("Station_id"), allDataFrame("Geo_cd")))
- newDataFrame = newDataFrame.withColumn("modified_Artist_id",  fillNullValueArtistId(newDataFrame("Song_id"), newDataFrame("Artist_id")))
+ newDataFrame = newDataFrame.withColumn("modified_Artist_id",  fillNullValueArtistId(newDataFrame("Songs_id"), newDataFrame("Artist_id")))
  newDataFrame = newDataFrame.withColumn("follower",  findFollowers(newDataFrame("User_id"), newDataFrame("modified_Artist_id")))
  newDataFrame = newDataFrame.withColumn("subscribed", findSubscribers(newDataFrame("User_id"), newDataFrame("Start_ts")))
- newDataFrame = newDataFrame.withColumn("isValid", validateRecords (newDataFrame("User_id"), newDataFrame("Song_id"), newDataFrame("modified_Artist_id"),  newDataFrame("modified_Geo_cd"), newDataFrame("Timestamp"), newDataFrame("Start_ts"), newDataFrame("End_ts")))
+ newDataFrame = newDataFrame.withColumn("isValid", validateRecords (newDataFrame("User_id"), newDataFrame("Songs_id"), newDataFrame("modified_Artist_id"),  newDataFrame("modified_Geo_cd"), newDataFrame("Timestamp"), newDataFrame("Start_ts"), newDataFrame("End_ts")))
 
  return newDataFrame
 }
